4af38a82b8e8a4853b3ebbef3bff7b85fc4b8e5d
negraodenio <negraodenio@gmail.com>
2026-02-16 12:44:15 +0000

    chore: update footer watermark to Made by Ia4all.eu

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,12 +2,12 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.dyad.sh/"
+        href="https://ia4all.eu"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        CRMia - Desenvolvido com Dyad
+        CRMia - Made by Ia4all.eu
       </a>
     </div>
   );
